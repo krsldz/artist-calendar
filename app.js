ffb6950cbdd56c1ed00ca3c56f4594db573bb76a
Kristina Eldzarova <keldzarova21@mlg.ru>
2024-02-15 18:19:10 +0300

    base server settings are added

---
+++ app.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT ?? 4001;
+
+app.listen(PORT, () => console.log(`Server is up on Port ${PORT}`));
